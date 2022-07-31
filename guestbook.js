6fa488cef51724acc156ae33608a1b0f5592e134
toml709 <toml709@gmail.com>
2022-07-31 17:24:13 -0600

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://https://bb833891.us-south.apigw.appdomain.cloud/guestbook/guestbook';
+const apiUrl = 'https://13e6a88c.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
